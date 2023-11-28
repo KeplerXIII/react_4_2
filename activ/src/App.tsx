86baf506ec34a42d76b210f7203bdbe0fae94fb1
Alexander Pushkin (Home) <a.v.pushkin@gmail.com>
2023-11-28 16:17:21 +0300

    upd

--- activ/src/App.tsx
+++ activ/src/App.tsx
@@ -1,16 +1,34 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+
+  // type Work = {
+  //   date: Date,
+  //   dist: number
+  // }
+
+  [works, changeWork] = useState([{date: '15.10.1988', dist: 5}])
+
+  const submitHendler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    console.log(e.target)
+  }
+
   return (
     <>
-      <form>
+    <div className='form-input'>
+      <form onSubmit={submitHendler}>
         <label htmlFor="date">Дата (ДД.ММ.ГГ)</label>
         <input type='date' name="date" />
         <label htmlFor="score">Пройдено км.</label>
         <input type="number" name="score" />
         <button type="submit">Добавить</button>
       </form>
+    </div>
+    <div className='work-list'>
+
+    </div>
     </>
   )
 }
